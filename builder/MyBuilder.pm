3523319457e04906349c06bb85c00c95bf3fec1d
ysasaki <aloelight@gmail.com>
2013-06-16 20:36:13 +0900

    Includes libsass

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -4,10 +4,11 @@ use warnings;
 use parent qw(Module::Build);
 
 sub new {
-    my ($self, %args) = @_;
+    my ( $self, %args ) = @_;
     $self->SUPER::new(
         %args,
-        extra_linker_flags => ['-lsass'],
+        extra_compiler_flags => [ '-x' => 'c++' ],
+        c_source             => ['libsass'],
     );
 }
 
